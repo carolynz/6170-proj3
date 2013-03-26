14042686722053d6636f42368a3cd7a15dd756f4
Carolyn Zhang <czhang92@gmail.com>
2013-03-25 23:58:24 -0400

    hello world running

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Project 3: Network Stickies
 
 ###Heroku URL
 
-Please put it here: http://sampleapp.herokuapp.com
+Please put it here: http://tranquil-inlet-4297.herokuapp.com/ 
 
 ###Notes
 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarolynzProj3::Application.config.secret_token = 'e3b39e51a505acb15ac357fcda6aa9885aeb9bab70958c98d8e0723cdee2f500a48d2d4232c3437dc675c0fd28e7299c07f3448a7fdb10c0d82424dd3a1da238'
