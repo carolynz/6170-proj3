8f213d9c031ec3c100dc43ca4f758b8c0e58933b
Carolyn Zhang <czhang92@gmail.com>
2013-04-18 02:59:28 -0400

    more specs

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -136,11 +136,28 @@ $(document).ready(function(){
       };      
     });
 
+ /**************************
+  * TOOLBAR CLICK HANDLING *
+  **************************/
+  // Open toolbar when the pencil icon is clicked
+  $('#notes').on('click', '.note_edit_class', function(){
+      if ($('.toolbar')[0]){
+        $('.toolbar').remove();
+      }
+      $(this).parent().append(toolbar);
+    });
   $("#notes").on('mousedown', '.toolbar', function(e){
       // Prevent mousedowns from taking focus off of contenteditable
       e.preventDefault();
-    })
-    .on('click','.style', function(e){
+    }).on('click','.dropdown-toggle', function(e){
+      e.preventDefault();
+      return false;
+    });
+
+  /***********************
+  * FONT CHANGE HANDLING *
+  ************************/  
+  $("#notes").on('click','.style', function(e){
       e.stopPropagation();
       var newStyle = "style-"+$(this).attr('id');
       var note = $(this).closest('.note');
@@ -155,75 +172,62 @@ $(document).ready(function(){
                  // console.log('style updated');
                }
       });
-    })
-    .on('click','.align', function(e){
-      // console.log('align clicked');
-      // console.log($(this));
-      // console.log(e);
-      // console.log('e.target: ');
-      // console.log(e.target);
-      e.stopPropagation();
-      var newAlign = "align-"+$(this).attr('id');
-      var note = $(this).closest('.note');
-      var noteId = note.attr('id').replace(/^\D+/g, '');
-      note.removeClassRegex(/^align-/).addClass(newAlign);
-      $.ajax({ url: '/notes/'+noteId,
-               type: 'PUT',
-               data: { note: { id: noteId, align: newAlign } },
-               success: function() {
-                 // console.log('alignment updated');
-               }
-      });
-    })
-    .on('click','.dropdown-toggle', function(e){
-      e.preventDefault();
-      return false;
     });
 
-    // Handle note hover events
-    $('#notes').on('mouseenter', '.note', function(){
-      // console.log('mouseentered note');
-      $(this).children(".note_delete_class").show();
-      if (!($(this).children('.toolbar')[0])){
-        $(this).children(".note_edit_class").show();
-      }
-    })
-    .on('mouseleave', '.note', function(){
-      $(this).children(".note_delete_class").hide();
-      $(this).children(".note_edit_class").hide();
+  /*********************
+  * ALIGNMENT HANDLING *
+  **********************/ 
+  $("#notes").on('click','.align', function(e){
+    e.stopPropagation();
+    var newAlign = "align-"+$(this).attr('id');
+    var note = $(this).closest('.note');
+    var noteId = note.attr('id').replace(/^\D+/g, '');
+    note.removeClassRegex(/^align-/).addClass(newAlign);
+    $.ajax({ url: '/notes/'+noteId,
+             type: 'PUT',
+             data: { note: { id: noteId, align: newAlign } },
+             success: function() {
+               // console.log('alignment updated');
+             }
     });
+  });
 
-    $('#notes').on('click', '.note_edit_class', function(){
-      if ($('.toolbar')[0]){
-        $('.toolbar').remove();
-      }
-      $(this).parent().append(toolbar);
-    });
+  // Handle note hover events
+  $('#notes').on('mouseenter', '.note', function(){
+    $(this).children(".note_delete_class").show();
+    if (!($(this).children('.toolbar')[0])){
+      $(this).children(".note_edit_class").show();
+    }
+  })
+  .on('mouseleave', '.note', function(){
+    $(this).children(".note_delete_class").hide();
+    $(this).children(".note_edit_class").hide();
+  });
 
-    // Handle note contents events
-    $('#notes').on('focus', '.note_contents', function(){
-      // If there are open toolbars elsewhere,
-      // remove them from the DOM
-      if ($('.toolbar')[0]){
-        $('.toolbar').remove();
-      }
-    })
-    .on('blur', '.note_contents', function(){
-      // console.log('note_contents blurred!');
-          var contents = $(this).contents;
-          var newText = $(this).html();
-          // console.log(newText);
-          var noteId = $(this).closest('.note').attr('id').replace(/^\D+/g, '');
-          $.ajax({ url: '/notes/'+noteId,
-                   type: 'PUT',
-                   data: { note: { id: noteId, contents: newText } },
-                   success: function() {
-                      // console.log('content update success!');
-                   }
-          });
-    });
+  // Handle note contents events
+  $('#notes').on('focus', '.note_contents', function(){
+    // If there are open toolbars elsewhere,
+    // remove them from the DOM
+    if ($('.toolbar')[0]){
+      $('.toolbar').remove();
+    }
+  })
+  .on('blur', '.note_contents', function(){
+        var contents = $(this).contents;
+        var newText = $(this).html();
+        // console.log(newText);
+        var noteId = $(this).closest('.note').attr('id').replace(/^\D+/g, '');
+        $.ajax({ url: '/notes/'+noteId,
+                 type: 'PUT',
+                 data: { note: { id: noteId, contents: newText } },
+                 success: function() {
+                    // console.log('content update success!');
+                 }
+        });
+  });
 
-
+  // Isotope is a jQuery plugin used for the gridlike effect
+  // For more information, see: isotope.metafizzy.co
   $('#notes').isotope({
     // options for jquery isotope plugin
     masonry: {
@@ -231,7 +235,6 @@ $(document).ready(function(){
     },
     itemSelector : '.note',
   });    
-
 });
 
 // Function to remove classes given a regex pattern
